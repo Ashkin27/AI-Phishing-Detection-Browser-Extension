5d34047208f686378a1a33121c57dc854ee3e0e0
Ashkin27 <ashkin27june@gmail.com>
2026-07-25 13:35:29 +0530

    Display current website URL in extension popup

--- extension/popup.js
+++ extension/popup.js
@@ -1,2 +1,12 @@
-document.getElementById("risk")
-.innerText = "Risk Score: 0";
+chrome.tabs.query(
+{
+active:true,
+currentWindow:true
+},
+function(tabs){
+
+const currentURL=tabs[0].url;
+
+document.getElementById("url").textContent=currentURL;
+
+});
